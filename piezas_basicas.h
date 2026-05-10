0fac997b91562e422a775cf3ccfa7ef5154626b7
Adrian <adrian.cespedes.l@utec.edu.pe>
2026-05-10 17:56:04 -0500

    Piezasbasicash

--- piezas_basicas.h
+++ piezas_basicas.h
@@ -1,8 +1,16 @@
-//
-// Created by AdrianCespedes on 7/05/2026.
-//
-
 #ifndef AJEDREZ_PIEZAS_BASICAS_H
 #define AJEDREZ_PIEZAS_BASICAS_H
 
-#endif //AJEDREZ_PIEZAS_BASICAS_H
+#include "constantes.h"
+
+// Peón
+bool validarPeon(char tablero[8][8], int fo, int co, int fd, int cd, int turno);
+void coronarPeon(char tablero[8][8], int fila, int col);
+
+// Torre
+bool validarTorre(char tablero[8][8], int fo, int co, int fd, int cd);
+
+// Alfil
+bool validarAlfil(char tablero[8][8], int fo, int co, int fd, int cd);
+
+#endif //AJEDREZ_PIEZAS_BASICAS_H holaadjfoefef
